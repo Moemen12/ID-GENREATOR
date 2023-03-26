2c5c9b6e1a5ed9d642e3117ddd78a603d24be964
moemen saadeh <99843903+Moemen12@users.noreply.github.com>
2023-03-26 22:15:56 +0300

    Update generator.php

--- generator.php
+++ generator.php
@@ -2,20 +2,29 @@
 
 
  function generateUniqueId($table, $column) {
+  
             // Check if the required arguments are provided
+  
             if (empty($table) || empty($column)) {
                 throw new InvalidArgumentException('Missing required arguments');
             }
+  
             $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  
             $id = '';
             $unique = false;
+  
             while (!$unique) {
                 $id = '';
                 for ($i = 0; $i < 25; $i++) {
                     $id .= $characters[rand(0, strlen($characters) - 1)];
                 }
-                // Check if the ID already exists in the database
-                $count = DB::table($table)->where($column, $id)->count();
+                // Check if the ID already exists in the database (laravel)
+                   $count = DB::table($table)->where($column, $id)->count();
+             
+             
+                // Check if the ID already exists in the database (native php)
+                // $count = SELECT COUNT(*) FROM table WHERE column = id;
                 if ($count == 0) {
                     $unique = true;
                 }
