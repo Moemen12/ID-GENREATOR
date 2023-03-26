40bc9c3e117bd5dc6a9e363503a4b3d6c3752804
moemen saadeh <99843903+Moemen12@users.noreply.github.com>
2023-03-26 14:49:21 +0300

    Create generator.php

---
+++ src/generator.php
@@ -0,0 +1,26 @@
+<?php
+
+ function generateUniqueId($table, $column) {
+            // Check if the required arguments are provided
+            if (empty($table) || empty($column)) {
+                throw new InvalidArgumentException('Missing required arguments');
+            }
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $id = '';
+            $unique = false;
+            while (!$unique) {
+                $id = '';
+                for ($i = 0; $i < 25; $i++) {
+                    $id .= $characters[rand(0, strlen($characters) - 1)];
+                }
+                // Check if the ID already exists in the database
+                $count = DB::table($table)->where($column, $id)->count();
+                if ($count == 0) {
+                    $unique = true;
+                }
+            }
+            return $id;
+        }
+        
+
+?>
